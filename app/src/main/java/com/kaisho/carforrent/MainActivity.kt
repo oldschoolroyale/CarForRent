d45f44ec7db808825fcf92db888a0426c4f08a9a
Kairat <kairatnurakhmet@gmail.com>
2020-10-14 00:43:38 +0200

    Changed XML for first page

--- app/src/main/java/com/kaisho/carforrent/MainActivity.kt
+++ app/src/main/java/com/kaisho/carforrent/MainActivity.kt
@@ -2,6 +2,7 @@ package com.kaisho.carforrent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -9,9 +10,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        btnOne.setOnClickListener {
-            Toast.makeText(applicationContext, "AAA", Toast.LENGTH_SHORT).show()
-        }
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
     }
 }
